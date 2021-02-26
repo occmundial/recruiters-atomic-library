8bdba17c3609ba3e8b4f882230ff9b6232a6f029
moisesrh <irojas@occ.com.mx>
2021-02-25 20:51:10 -0600

    updates for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-const debug = process.env.NODE_ENV !== "production";
-
 module.exports = {
   exportPathMap: function () {
     return {
       "/": { page: "/" },
     }
   },
-  assetPrefix: !debug ? '/recruiters-atomic-library/' : '',
-  basePath: !debug ? '/recruiters-atomic-library' : '',
+  assetPrefix: '/recruiters-atomic-library/',
+  basePath: '/recruiters-atomic-library',
 }
