e9812df398f495c1f9555b26842f9dd51072d4b4
moisesrh <irojas@occ.com.mx>
2021-02-25 20:45:45 -0600

    Updates

---
+++ _next/static/pdhwCXkQLX9GIuoayMio_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-c28389af7ed6a2cd8ff1.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-03f3939929daf5df3091.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
