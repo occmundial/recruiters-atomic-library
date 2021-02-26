3b68fcf3419ef0014e18fed220705e9578b743ab
moisesrh <irojas@occ.com.mx>
2021-02-25 20:26:32 -0600

    bye webpack

--- next.config.js
+++ next.config.js
@@ -9,13 +9,4 @@ module.exports = {
   assetPrefix: !debug ? '/recruiters-atomic-library/' : '',
   useFileSystemPublicRoutes: false,
   basePath: !debug ? '/recruiters-atomic-library' : '',
-  webpack: config => {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
-      }),
-    )
-
-    return config
-  },
 }
