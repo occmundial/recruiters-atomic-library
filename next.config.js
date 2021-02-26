bb13663c9df6b6fe3f4e2445436463d481754cdc
moisesrh <irojas@occ.com.mx>
2021-02-25 21:27:27 -0600

    basepath

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
   assetPrefix: !debug ? 'https://occmundial.github.io/recruiters-atomic-library/' : '',
-  basePath: !debug ? 'https://occmundial.github.io/recruiters-atomic-library' : '',
+  basePath: !debug ? '/recruiters-atomic-library' : '',
 }
